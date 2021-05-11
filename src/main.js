eb256d65a6fdfba72761b971de38afad674b8b7f
waleedomega <waleedomega@hotmail.com>
2021-05-11 17:37:06 +0200

    adding plugins and dependencies

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 // eslint-disable-next-line no-unused-vars
 const app = new Vue({
+  router,
+  store,
+
   render: function (hyperscript) {
     return hyperscript(App);
   }
